8efa32406bdb9f88ca603a8fd91e6b757454e615
Efraim John <kasengaefraim@gmail.com>
2025-04-20 15:48:34 +0300

    routes fix

--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -1,3 +1,13 @@
 import express from "express";
 const router = express.Router();
+
+router.post("/", async (req, res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+})
+
+
 export default router;
