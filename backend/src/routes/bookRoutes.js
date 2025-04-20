26291ec13909e30ba84b34f06542870c9793b903
Efraim John <kasengaefraim@gmail.com>
2025-04-20 16:38:35 +0300

    routes fix

--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -1,13 +1,62 @@
 import express from "express";
+import fs from "fs/promises";
+import path from "path";
+import protectRoute from "../middleware/auth.middleware.js";
+import Book from "../models/Book.js";
 const router = express.Router();
 
-router.post("/", async (req, res)=>{
+router.post("/", protectRoute, async (req, res) => {
     try {
-        
+
+        const { title, caption, rating, image } = req.body;
+        if (!image || !title || !caption || !rating) {
+            return res.status(400).json({ message: "please provide all fields" });
+        }
+        await fs.writeFile(
+            path.join(__dirname, "images",)
+        );
+
+        const imageUrl = '';
+        const newBook = new Book({
+            title,
+            caption,
+            rating,
+            image: imageUrl,
+            user: req.user._id
+        });
+        await newBook.save();
+        res.status(201).json(newBook);
+
     } catch (error) {
-        
+        console.log("Error creating book", error);
+        res.status(500).json({ message: error.message });
     }
 })
 
+router.get("/", protectRoute, async (req, res) => {
+    try {
+
+        const page = req.query.page || 1;
+        const limit = req.query.limit || 5;
+        const skip = (page - 1) * limit;
+
+        const books = await Book.find()
+            .sort({ createdAt: -1 })
+            .skip(skip)
+            .limit(limit)
+            .populate("user", "username profileImage");
+        const totalBooks = await Book.countDocuments();
+        res.send({
+            books,
+            currentPage: page,
+            totalBooks,
+            totalPages: Math.ceil(totalBooks / limit),
+        });
+
+    } catch (error) {
+        console.log("Error getting books", error);
+        res.status(500).json({ message: "internal server error" });
+    }
+})
 
 export default router;
